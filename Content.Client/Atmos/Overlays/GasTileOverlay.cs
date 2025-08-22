dddac0997c6d2712b7eb2d585f138d7f3c5a9b68
Janet Blackquill <uhhadd@gmail.com>
2025-08-22 14:26:37 -0400

    offset gas overlay

--- Content.Client/Atmos/Overlays/GasTileOverlay.cs
+++ Content.Client/Atmos/Overlays/GasTileOverlay.cs
@@ -46,6 +46,8 @@ namespace Content.Client.Atmos.Overlays
         private readonly int[] _fireFrameCounter = new int[FireStates];
         private readonly Texture[][] _fireFrames = new Texture[FireStates][];
 
+        private static readonly Vector2 TileOffset = new(0f, 0.5f); // Stellar - wallening
+
         private int _gasCount;
 
         public const int GasOverlayZIndex = (int) Shared.DrawDepth.DrawDepth.Effects; // Under ghosts, above mostly everything else
@@ -227,7 +229,7 @@ namespace Content.Client.Atmos.Overlays
                             {
                                 var opacity = gas.Opacity[i];
                                 if (opacity > 0)
-                                    state.drawHandle.DrawTexture(state.frames[i][state.frameCounter[i]], tilePosition, Color.White.WithAlpha(opacity));
+                                    state.drawHandle.DrawTexture(state.frames[i][state.frameCounter[i]], tilePosition + TileOffset, Color.White.WithAlpha(opacity)); // Stellar - wallening
                             }
                         }
                     }
@@ -249,7 +251,7 @@ namespace Content.Client.Atmos.Overlays
 
                             var fireState = gas.FireState - 1;
                             var texture = state.fireFrames[fireState][state.fireFrameCounter[fireState]];
-                            state.drawHandle.DrawTexture(texture, index);
+                            state.drawHandle.DrawTexture(texture, index + TileOffset); // Stellar - wallening
                         }
                     }
 
@@ -290,7 +292,7 @@ namespace Content.Client.Atmos.Overlays
                         var opacity = atmos.OverlayData.Opacity[i];
 
                         if (opacity > 0)
-                            handle.DrawTexture(_frames[i][_frameCounter[i]], tilePosition, Color.White.WithAlpha(opacity));
+                            handle.DrawTexture(_frames[i][_frameCounter[i]], tilePosition + TileOffset, Color.White.WithAlpha(opacity)); // Stellar - wallening
                     }
                 }
             }
